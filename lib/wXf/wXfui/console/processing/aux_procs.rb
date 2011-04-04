750efbbc4eab680cbf1c049d5ca3687a4426ceb1
cktricky <cktricky@192.168.1.106>
2011-04-03 22:37:58 -0400

    LFILE (already avail in "server") is now available in auxiliary and exploit modules (file-based). We perform automatic translation and reset. Additionally, a glitch where file based exploits options weren't appearing has been repaired.

--- lib/wXf/wXfui/console/processing/aux_procs.rb
+++ lib/wXf/wXfui/console/processing/aux_procs.rb
@@ -9,6 +9,8 @@ module Processing
   
   class AuxiliaryProcs 
     
+    attr_accessor :opts
+    
     include WXf::WXfui::Console::Operations::ModuleCommandOperator
 
 
@@ -24,16 +26,59 @@ module Processing
     def avail_args; {"run" => "Runs the module"}; end
     
     
+        
+    #
+    # This method determines if an lfile option has been set
+    #      
+    def lfile_check(activity)
+      return unless activity.respond_to?('datahash')
+        val = nil
+          if activity.datahash.has_key?('LFILE')
+            val = activity.datahash['LFILE']
+          else
+            val = nil   
+          end
+        if !val.nil?
+          lfile_translation(activity, val)
+        end
+    end  
+    
+    
+    #
+    # Method to translate lfile to real LFILE
+    #
+    def lfile_translation(activity, val=nil)
+      inst = val
+      self.opts['LFILE'] = val
+        if control.framework.modules.lfile_load_list.has_key?(inst)        
+          name = control.framework.modules.lfile_load_list[inst]
+          activity.datahash['LFILE'] = name
+        end
+    end
+    
+    
+    #
+    # We need to reset the lfile appearance (think - show options)
+    #
+    def lfile_reset(activity)
+      return unless activity.respond_to?('datahash')
+        if activity.datahash.has_key?('LFILE') and self.opts.has_key?('LFILE')
+          activity.datahash['LFILE'] = self.opts['LFILE']
+        end  
+    end
+        
     #
     # Checks and error handling, nm at the moment tho :-)
     #
-    def arg_run(*cmd)     
-      begin
-        activity.run
-     rescue => $!
-        print("The following error occurred: #{$!}" + "\n")
-     end   
-   
+    def arg_run(*cmd)
+      self.opts = {}
+      lfile_check(activity)
+        begin
+          activity.run
+        rescue => $!
+          print("The following error occurred: #{$!}" + "\n")
+        end
+      lfile_reset(activity)     
     end  
      
   end
--- modules/auxiliary/enum/dirchex.rb
+++ modules/auxiliary/enum/dirchex.rb
@@ -7,30 +7,43 @@ class WebXploit < WXf::WXfmod_Factory::Auxiliary
 
   def initialize
       super(
-      
+        'Name'        => 'DIRCHEX',
+        'Version'     => '1.0',
+        'Description' => %q{
+          Command line version of the program DirChex by k3r0s1n3 and cktricky.                        },
+        'References'  =>
+        [
+          [ 'URL', 'http://code.google.com/p/dirchex/' ],
+        ],
+        'Author'      => [ 'CKTRICKY' ],
+        'License'     => WXF_LICENSE
 
-    'Name'        => 'DIRCHEX',
-    'Version'     => '1.0',
-    'Description' => %q{
-     COMING SOON WE PROMISE!
-                        },
-    'References'  =>
-     [
-        [ 'URL', 'http://code.google.com/p/dirchex/' ],
-     ],
-    'Author'      => [ 'CKTRICKY' ],
-    'License'     => WXF_LICENSE
-
-        
       )
-        
+   
+      init_opts([
+        OptString.new('METHOD', [true, "Enter either GET or PUT HTTP Methods", "GET"]),
+        OptString.new('UA',     [true, "Please enter a User Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:2.0b9pre) Gecko/20110111 Firefox/4.0b9pre" ]),
+        OptString.new('CONTENTTYPE', [false, "Enter a content-type, ONLY if using PUT method", "application/javascript"]),
+        OptString.new('PUTFILE', [false, "Enter the name of the file to PUT (only if using PUT)", "test_file"]),
+        OptString.new('FILECONTENT', [false, "Enter text to put into the file (only is using PUT)", "test text within test_file"]),
+        OptString.new('LFILE', [true, "Enter the local file to read from", ""])
+      ])
   
   end
   
   def run
-    
-    prnt_dbg(" Coming Soon")
-    
+   
+=begin    
+    res = mech_req({
+     'method' => datahash[''],
+     'UA' => check,
+     'RURL'=> rurl,
+     'PROXY_ADDR' => proxya,
+     'PROXY_PORT' => proxyp,
+     'REDIRECT' => 'false',
+               }                                                                 
+             )
+=end
   end
   
 end
