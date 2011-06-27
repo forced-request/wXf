d6104919a73b9f28c5a901e3533ab58f115461a2
cktricky <cktricky@192.168.1.178>
2011-06-27 12:08:22 -0400

    clean-up

--- lib/wXf/wXfassists/general/savon_req.rb
+++ lib/wXf/wXfassists/general/savon_req.rb
@@ -27,110 +27,113 @@ module SavonReq
              ])
              
       end
-           
-     
+      
+      
+      #
+      # Remote URL option
+      #    
       def rurl
         datahash['RURL']
       end
 
+      
+     #
+     # Proxy port option
+     # 
      def proxyp
        datahash['PROXYP']
      end
 
+     
+     #
+     # Proxy address option
+     #
      def proxya
        datahash['PROXYA']
      end
    
    
-  #
-  # Method which returns true if option isn't either a blank string or n  
-  #  
-  def exists?(opt)    
-  
-    if (opt and opt != '' and not opt == nil)
-       return true
-    else
-       return false         
-    end
-  
-  end
- 
-  
-  #
-  # This method can be used to ensure that if only value should 
-  # ...is required, only one is entered.
-  #
-  def eliminator(arg1, arg2)
-    
-    if exists?(arg1) and not exists?(arg2)
-      return arg1
-    elsif exists?(arg2) and not exists?(arg1)
-      return arg2
-    else        
-     prnt_err(" Please ensure that both #{arg1} and #{arg2} aren't set")    
+    #
+    # Method which returns true if option isn't either a blank string or n  
+    #  
+    def exists?(opt)      
+        if (opt and opt != '' and not opt == nil)
+          return true
+        else
+          return false
+        end
     end
     
-  end
-  
-  
     
-  #
-  # This method instantiates a client object for the developer to work with.
-  # 
-  #   
-  def simple_savon_client(opts={})
-    
-    rurl = opts['RURL']  
-    begin             
-     WAx::WAxHTTPLibs::Savon::Client.new("#{rurl}")
-    rescue => $!
-      prnt_err(" SavonReq Error: #{$!}")
+    #
+    # This method can be used to ensure that if only value should 
+    # ...is required, only one is entered.
+    #
+    def eliminator(arg1, arg2)
+      if exists?(arg1) and not exists?(arg2)
+        return arg1
+      elsif exists?(arg2) and not exists?(arg1)
+        return arg2
+      else
+        prnt_err(" Please ensure that both #{arg1} and #{arg2} aren't set")
+      end
     end
     
-  end
-  
-  
-  #
-  #
-  #
-  def single_action_req(opts={})
-    rurl = opts['RURL'] || ''
-    rparams = opts['RPARAMS'] || {}
-    raction = opts['RACTION'] || nil
-    headers = opts['HEADERS'] || {}
-    basic_user = opts['USER'] || nil
-    basic_pass =  opts['PASS'] || nil
-    gzip = opts['GZIP'] || true
+    
+    #
+    # This method instantiates a client object for the developer to work with.
+    # 
+    #   
+    def simple_savon_client(opts={})
+      rurl = opts['RURL']
+      begin
+        WAx::WAxHTTPLibs::Savon::Client.new("#{rurl}")
+      rescue => $!
+        prnt_err(" SavonReq Error: #{$!}")
+      end
+    end
+    
+    
+    #
+    # This makes a request with only a single action
+    #
+    def single_action_req(opts={})
+      rurl = opts['RURL'] || ''
+      rparams = opts['RPARAMS'] || {}
+      raction = opts['RACTION'] || nil
+      proxyp = opts['PROXYP']
+      proxya = opts['PROXYA']
+      headers = opts['HEADERS'] || {}
+      basic_user = opts['USER'] || nil
+      basic_pass =  opts['PASS'] || nil
+      gzip = opts['GZIP'] || true
+        
+      begin
+        client = WAx::WAxHTTPLibs::Savon::Client.new("#{rurl}")
+        
+        if exists?(basic_user) and exists?(basic_pass)
+            client.request.basic_auth("#{basic_user}", "#{basic_pass}")
+        end
+           
+        if exists?(proxyp) and exists?(proxya)
+          client.request.proxy.port = "#{proxyp}"
+          client.request.proxy.host = "#{proxya}"
+        end
+    
+        headers.each {|k,v| client.request.headers[k] = v }
    
-       
-   begin             
-    client = WAx::WAxHTTPLibs::Savon::Client.new("#{rurl}")
-    
-    if exists?(basic_user) and exists?(basic_pass)
-    client.request.basic_auth("#{basic_user}", "#{basic_pass}")
-    end
-    
-    if exists?(proxyp) and exists?(proxya)
-    client.request.proxy.port = "#{proxyp}"
-    client.request.proxy.host = "#{proxya}" 
-    end
-    headers.each {|k,v| client.request.headers[k] = v }
-   
-    if exists?(raction) and exists?(rparams)
-    eval("
-      client.#{raction} { |soap|
-         
-          soap.body = rparams
-      }
-    ")
-    else
-      prnt_err(" You forgot the RACTION or PARAM/VALUE option(s)!")
-    end
-    
-    rescue => $!
-      prnt_err(" SavonReq Error: #{$!}")
-    end
-
+        if exists?(raction) and exists?(rparams)
+          eval("
+            client.#{raction} { |soap|             
+              soap.body = rparams
+            }
+          ")
+        else
+          prnt_err(" You forgot the RACTION or PARAM/VALUE option(s)!")
+        end
+      rescue => $!
+        prnt_err(" SavonReq Error: #{$!}")
+      end
    end
     
 end
