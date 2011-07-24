f653e8f6d03dc4f65e1a602ecd5d3f9e9ebfb06a
Ken Johnson <cktricky@gmail.com>
2011-07-23 23:47:58 -0400

    Joomla version detection added

--- modules/auxiliary/enum/joomla_version_scanner.rb
+++ modules/auxiliary/enum/joomla_version_scanner.rb
@@ -7,25 +7,22 @@ class WebXploit < WXf::WXfmod_Factory::Auxiliary
 
             'Name'        => 'Joomla Version Scanner',
             'Description' => %q{
-         
-                            
-               
-            },
-            'References'  =>
-             [
-              
-             ],
+              This is a basic Joomla version detection module. It takes advantage of the Joomla auxiliary assist library                                     
+            },           
             'Author'      => [ 'CKTRICKY',],
             'License'     => WXF_LICENSE
   
     )
-          
-
+     
   end
   
   def run
-  
-  
+    version = detect_version
+    if version.nil?
+      prnt_err("No Joomla Version detected, sorry :-(")
+    else
+      prnt_plus("The Joomla Version detected was version: #{version}")
+    end
   end
 
 end
