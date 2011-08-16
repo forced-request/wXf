57d847a5984cd69f5a630834900337b93829ac7a
Ken Johnson <cktricky@gmail.com>
2011-08-16 13:05:12 -0400

    Wording change

--- modules/buby/html_comments_online.rb
+++ modules/buby/html_comments_online.rb
@@ -68,7 +68,7 @@ class WebXploit < WXf::WXfmod_Factory::Buby
     )
     
     init_opts([
-      OptString.new('RURL', [true, "Enter the remote url value", "http://www.example.com"])           
+      OptString.new('RURL', [true, "Enter the RURL from which we should extract", "http://www.example.com"])           
     ])
   end
 
