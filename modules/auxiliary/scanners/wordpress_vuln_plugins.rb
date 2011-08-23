b7f9bc381c46afdf450fcb73ebc4635b314049f0
Ken Johnson <cktricky@gmail.com>
2011-08-23 17:30:11 -0400

    slight modification to wordpress module

--- modules/auxiliary/scanners/wordpress_vuln_plugins.rb
+++ modules/auxiliary/scanners/wordpress_vuln_plugins.rb
@@ -22,7 +22,26 @@ class WebXploit < WXf::WXfmod_Factory::Auxiliary
       
   end
   
+  def print_found(list)
+    list_items = list.sort
+       # Display the commands
+         tbl = WXf::WXfui::Console::Prints::PrintTable.new(
+           'Justify'  => 4,             
+           'Columns' => 
+           [
+             'Response Code',
+             'Plugin Name',
+             'Vulnerability',
+             'Reference'
+           ])
+         list_items.each do |code, found, index, name, vuln, ref|  
+           tbl.add_ritems([code, name, vuln, ref])
+         end
+       tbl.prnt    
+  end
+  
   def run
+    list = []
     enumerate_vuln_plugins(3, true).each do |row|
      code = row[0]
      found = row[1]
@@ -32,11 +51,17 @@ class WebXploit < WXf::WXfmod_Factory::Auxiliary
      ref = row[5]
     
      if found
-       prnt_plus("Found: plugin #{name}")
-       prnt_plus("#{rurl}/wp-content/plugins/#{name} via response code: #{code}")     
+      list.push(row) 
      end
     
     end
+    
+    if !list.empty?
+      print_found(list)
+    else
+      prnt_err("No vulnerable plugins discovered")
+    end
+    
   end
 
 end
