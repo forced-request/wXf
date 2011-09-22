bab0e61772362c5ce575cf47259ad0e4c533a19b
Ken Johnson <cktricky@gmail.com>
2011-09-22 12:35:23 -0400

    History survives console restart

--- jlib/wXf/wXfui/console/shell_func.rb
+++ jlib/wXf/wXfui/console/shell_func.rb
@@ -1,3 +1,4 @@
 require 'wXf/wXfui/console/shell_func/file'
+require 'wXf/wXfui/console/shell_func/history'
 require 'wXf/wXfui/console/shell_func/reader'
 require 'wXf/wXfui/console/shell_func/shell'
--- jlib/wXf/wXfui/console/shell_func/reader.rb
+++ jlib/wXf/wXfui/console/shell_func/reader.rb
@@ -20,6 +20,8 @@ module Shell_Func
    def initialize(tab_completion=nil)
      rl_check
      rl_tabcomp_engine(tab_completion)
+     history = History.new
+     @file = File.open(history.history_file, "a")
    end
    
    
@@ -28,9 +30,13 @@ module Shell_Func
    #   
    def grab        
      line = nil
-    begin          
-     line = ::Readline.readline(prm, true)
-    end
+     begin          
+      line = ::Readline.readline(prm, true)
+      if not line =~ /^\s*$/
+        @file.puts(line)
+      end
+      end
+    return line
    end
       
  
