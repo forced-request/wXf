eca36440aff8c4171bcbb76976e1c53e83d2d8b3
Ken Johnson <cktricky@gmail.com>
2011-10-06 09:30:03 -0400

    removing code that was used for testing.

--- jlib/wXfgui/decision_tree.rb
+++ jlib/wXfgui/decision_tree.rb
@@ -1,4 +1,3 @@
 require 'wXfgui/decision_tree/decision_tree_factory'
-require 'wXfgui/decision_tree/decision_tree_list'
 require 'wXfgui/decision_tree/decision_tree_model'
 require 'wXfgui/decision_tree/decision_tree_popup'
