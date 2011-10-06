d308d7206255c07240bae4ceb507f3e7d0607987
Ken Johnson <cktricky@gmail.com>
2011-10-06 01:44:07 -0400

    new gui stuff

---
+++ jlib/wXfgui/database_manager.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env jruby
+
+require 'rubygems'
+require 'jdbc/sqlite3'
+require  'find'
+
+require 'java'
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.sqlite.JDBC;
+
+module WxfGui
+class DatabaseManager
+  def initialize(name)
+    @name = name
+    init_db
+   
+  end
+  
+  def init_db
+    home_dir = ENV['HOME']
+    wXf_home_dir = "#{home_dir}/.wXf"    
+    connection = DriverManager.getConnection("jdbc:sqlite:#{wXf_home_dir}/#{@name}.db")
+  end 
+end 
+
+end
