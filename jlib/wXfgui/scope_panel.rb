8ad207dbdaf2ad9c70852e314bb1d03cb2a1c3bb
Ken Johnson <cktricky@gmail.com>
2011-10-07 21:43:36 -0400

    added a combo box and 3 textfields to the scope

--- jlib/wXfgui/scope_panel.rb
+++ jlib/wXfgui/scope_panel.rb
@@ -1,15 +1,45 @@
 #!/usr/bin/env jruby
 
-
+import java.awt.FlowLayout
+import java.awt.Dimension
 import javax.swing.JPanel
 import javax.swing.JScrollPane
 import javax.swing.JTable
+import javax.swing.JTextField
 import javax.swing.JCheckBox
+import javax.swing.JComboBox
 import javax.swing.JButton
 import java.awt.event.MouseListener
 import javax.swing.table.DefaultTableModel
 
 module WxfGui
+   
+class ScopePanelExtension < JPanel
+   
+   def initialize
+      super(FlowLayout.new(FlowLayout::LEFT))
+      init
+   end
+   
+   def init
+       combo_prefix = ["https", "http"]
+       combo_box = JComboBox.new
+       combo_prefix.each do |pre|
+           combo_box.addItem(pre)
+        end
+        
+       host_field = JTextField.new(14)
+       port_field = JTextField.new(5)
+       path_field = JTextField.new(22)
+       
+       add(combo_box)
+       add(host_field)
+       add(port_field)
+       add(path_field)
+       
+   end
+end
+   
 class ScopePanel < JPanel
   
    include MouseListener
@@ -21,21 +51,22 @@ class ScopePanel < JPanel
   
   def init
      
-        add_button  = JButton.new("add")
-        edit_button = JButton.new("edit")
-        rem_button  = JButton.new("remove")
+      
      
- 
-         m = DefaultTableModel.new
-         m.add_column("prefix")
-         m.add_column("host")
-         m.add_column("port")
-         m.add_column("path")
-         table = JTable.new(m)
-         table.setPreferredScrollableViewportSize(Dimension.new(500, 70))
-         table.setFillsViewportHeight(true)
+       spe = ScopePanelExtension.new
+       add_button  = JButton.new("add")
+       edit_button = JButton.new("edit")
+       rem_button  = JButton.new("remove")
+       m = DefaultTableModel.new
+       m.add_column("prefix")
+       m.add_column("host")
+       m.add_column("port")
+       m.add_column("path")
+       table = JTable.new(m)
+       table.setPreferredScrollableViewportSize(Dimension.new(500, 70))
+       table.setFillsViewportHeight(true)
         
-        jpane = JScrollPane.new(table)
+       jpane = JScrollPane.new(table)
       
       #
       # GROUP LAYOUT OPTIONS
@@ -52,6 +83,7 @@ class ScopePanel < JPanel
       sv1 = layout.createSequentialGroup
       sv2 = layout.createSequentialGroup
       sv3 = layout.createSequentialGroup
+      p  = layout.createParallelGroup
       p1 = layout.createParallelGroup
       p2 = layout.createParallelGroup
       p3 = layout.createParallelGroup
@@ -59,13 +91,19 @@ class ScopePanel < JPanel
       layout.setHorizontalGroup sh1
       layout.setVerticalGroup sv3
       
-      sh1.addComponent(jpane)
+      
+      p.addComponent(jpane)
+      p.addComponent(spe)
       p1.addComponent(add_button)
       p1.addComponent(edit_button)
       p1.addComponent(rem_button)     
+      sh1.addGroup(p)
       sh1.addGroup(p1)
       
       sv1.addComponent(jpane)
+      sv1.addComponent(spe)
+      sv1.addGroup(p3)
+      
       sv2.addComponent(add_button)
       sv2.addComponent(edit_button)
       sv2.addComponent(rem_button)
@@ -76,10 +114,6 @@ class ScopePanel < JPanel
       
       layout.linkSize SwingConstants::HORIZONTAL, 
           add_button, edit_button, rem_button
-      
-  
-   
-
       
   end
 end
