9c965dde5044e12107a168d8959e9596a224248e
Ken <cktricky@gmail.com>
2011-11-14 14:40:18 -0500

    Added a body length to the timthumb results, thinking about doing this for the other API calls.

--- jlib/wXf/wXfassists/auxiliary/wordpress/wordpress.rb
+++ jlib/wXf/wXfassists/auxiliary/wordpress/wordpress.rb
@@ -38,47 +38,46 @@ module Wordpress
     end
   
   
-    # 
-    # Enumerate all timthumb vulnerabilities
-    #
-    def enumerate_timthumb(timeout, prnt)
-     rows = []
-      vulns = fetch_wp_timthumb
-      vulns.each_with_index do |row, idx|
-        count = idx + 1
-        if (prnt)
-          prnt_gen("Requesting #{count} of #{vulns.length}")
-        end      
-        index = row[0]
-        name = row[1]
-        path = row[2]
-        ref = row[3]       
-        url = "#{rurl}/wp-content/themes/#{path}"  
-          res = mech_req({
-            'method'=> 'GET',
-            'RURL'  =>  url,   
-            'TIMEOUT' => timeout,
-          })
-  
-    puts "Sending req to #{url}"
-      
-        if res and res.respond_to?('code') and res.code == "200"
-          row.unshift(res.code.to_i, true)
-          rows << row
-        elsif rce_code == 403
-          row.unshift(rce_code, true)
-          rows << row
-        elsif res and res.respond_to?('code') and not res.code == "200"
-          row.unshift(res.code.to_i, false)
-          rows << row
-        elsif rce_code and not rce_code == 403
-          row.unshift(rce_code, false)
-          rows << row
-        end
-      end
-      return rows
-    end    
+  # 
+  # Enumerate all timthumb vulnerabilities
+  #
+  def enumerate_timthumb(timeout, prnt)
+   rows = []
+    vulns = fetch_wp_timthumb
+    vulns.each_with_index do |row, idx|
+      count = idx + 1
+      if (prnt)
+        prnt_gen("Requesting #{count} of #{vulns.length}")
+      end      
+      index = row[0]
+      name = row[1]
+      path = row[2]
+      ref = row[3]       
+      url = "#{rurl}/wp-content/themes/#{path}"  
+        res = mech_req({
+          'method'=> 'GET',
+          'RURL'  =>  url,   
+          'TIMEOUT' => timeout,
+        })
+
+  puts "Sending req to #{url}"
     
+      if res and res.respond_to?('code') and res.code == "200"
+        row.unshift(res.body.length, res.code.to_i, true)
+        rows << row
+      elsif rce_code == 403
+        row.unshift(rce_code, true)
+        rows << row
+      elsif res and res.respond_to?('code') and not res.code == "200"
+        row.unshift(res.body.length, res.code.to_i, false)
+        rows << row
+      elsif rce_code and not rce_code == 403
+        row.unshift(rce_code, false)
+        rows << row
+      end
+    end
+    return rows
+  end
     
   #
   # Enumerate site for all vulnerable wordpress plugins
