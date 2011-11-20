fd97587e891a381215ab9a1834bee13036c9da92
Ken Johnson <cktricky@gmail.com>
2011-11-20 18:10:34 -0500

    So for right now, SCA panel doesn't work, BUT, it DOES look right. Swing can be painful.

--- jlib/wXfgui/sca/panels/sca_panel.rb
+++ jlib/wXfgui/sca/panels/sca_panel.rb
@@ -4,19 +4,13 @@ require 'rubygems'
 require 'java'
 require 'find'
 
-begin
-   require 'to_regexp'
-rescue LoadError
-    print("\e[1;31m[wXf error]\e[0m Please ensure you have the following gems installed:\n")
-    print("1) to_regexp\n")
-end
-
 import javax.swing.ListSelectionModel
 import javax.swing.table.DefaultTableModel
 import javax.swing.JTable
 import javax.swing.JTabbedPane
 import javax.swing.SwingConstants
 import javax.swing.GroupLayout
+import java.awt.BorderLayout
 import java.awt.Color
 import javax.swing.JButton
 import java.awt.FlowLayout
@@ -32,6 +26,7 @@ import java.awt.event.MouseListener
 import java.awt.Dimension
 import javax.swing.JLabel
 
+
 class DataEntryPanel < JPanel
    
    def initialize
@@ -40,15 +35,54 @@ class DataEntryPanel < JPanel
    end
    
    def init
-   
-      # Labels
       
+      # Buttons
+      chooseDir = JButton.new("choose")
+      searchButton = JButton.new("search")
+   
+   
+      # Labels      
       l1 = JLabel.new("Directory to search")
-      l2 = JLabel.new("string to search for")
+      l2 = JLabel.new("String to search for")
       
       # Text Fields
-      @tf1 = JTextField.new(3)
-      @tf2 = JTextField.new(3)
+      @tf1 = JTextField.new(30)
+      @tf2 = JTextField.new(30)
+      
+      # Jpanel(s)
+      jp1 = JPanel.new(FlowLayout.new(FlowLayout::LEFT))
+      jp1.add(l1)
+      jp1.add(@tf1)
+      jp2 = JPanel.new(FlowLayout.new(FlowLayout::LEFT))
+      jp2.add(l2)
+      jp2.add(@tf2)
+   
+   
+      # Add listener actions to button
+      #
+      # Choose Directory Button
+      chooseDir.addActionListener do |e|
+        dir = JFileChooser.new
+        dir.setCurrentDirectory(java.io.File.new(Dir.pwd))
+        dir.setFileSelectionMode(JFileChooser::DIRECTORIES_ONLY)
+        ret = dir.showDialog @panel, "Choose Directory"
+        if  ret  == JFileChooser::APPROVE_OPTION
+            destDir = dir.getCurrentDirectory()
+            searchDir = destDir.to_s
+            @tf1.text = searchDir.length > 0 ? searchDir : ''
+        end 
+      end 
+      
+      # Search button
+      searchButton.addActionListener do |e|
+         if @tf1.text.length <= 0
+           error("Please enter a directory")
+         elsif @tf2.text.length <= 0
+           error("Please enter something to search for")
+         else
+            search
+         end
+      end
       
       #
       # GROUP LAYOUT OPTIONS
@@ -60,23 +94,43 @@ class DataEntryPanel < JPanel
       # Create sensible gaps in components (like buttons)
       layout.setAutoCreateGaps true
       layout.setAutoCreateContainerGaps true     
-   
-      sh1 = layout.createSequentialGroup
-      sv1 = layout.createSequentialGroup
-      p1 = layout.createParallelGroup
     
-      layout.setHorizontalGroup sh1
-      layout.setVerticalGroup sv1
+      sh1 = layout.createSequentialGroup
+      sh2 = layout.createSequentialGroup
+      sh3 = layout.createSequentialGroup
+      sv1 = layout.createSequentialGroup
+      sv2 = layout.createSequentialGroup
+      sv3 = layout.createSequentialGroup
+      p1 = layout.createParallelGroup
+      p2 = layout.createParallelGroup
+      p3 = layout.createParallelGroup
+      p4 = layout.createParallelGroup
+      p5 = layout.createParallelGroup
       
-      p1.addComponent(l1)
-      p1.addComponent(@tf1)
-      p1.addComponent(l2)
-      p1.addComponent(@tf2)
-      sh1.addGroup(p1)
-      sv1.addComponent(l1)
-      sv1.addComponent(@tf1)
-      sv1.addComponent(l2)
-      sv1.addComponent(@tf2)
+      layout.setHorizontalGroup sh3
+      layout.setVerticalGroup sv3
+      
+      
+      # Horizontal
+      p1.addComponent(jp1)
+      p1.addComponent(jp2)
+      p2.addComponent(chooseDir)
+      p2.addComponent(searchButton)
+      sh3.addGroup(p1)
+      sh3.addGroup(p2)
+      
+      
+      # Vertical
+      sv1.addComponent(jp1)
+      sv1.addComponent(jp2)
+      sv2.addComponent(chooseDir)
+      sv2.addComponent(searchButton)
+      p5.addGroup(sv1)
+      p5.addGroup(sv2)
+      sv3.addGroup(p5)
+      
+      layout.linkSize SwingConstants::HORIZONTAL, 
+          chooseDir, searchButton
 
    end
 end
@@ -126,70 +180,23 @@ class ScaPanel < JPanel
   end
   
   def init
-   
-      
      
       # Panels
       jp1 = DataEntryPanel.new()
-      #JPanel.new(FlowLayout.new(FlowLayout::LEFT))
-      #jp2 = JPanel.new(FlowLayout.new(FlowLayout::LEFT))
-      jp3 = JPanel.new(FlowLayout.new(FlowLayout::LEFT))
-      #  jp4 = JPanel.new(FlowLayout.new(FlowLayout::LEFT))
-      jp5 = JPanel.new(FlowLayout.new(FlowLayout::LEFT))
+      jp2 = JPanel.new(FlowLayout.new(FlowLayout::LEFT))
       
-    
-     
       # Table area
        @results_table = ResultsTable.new
        @table = JTable.new(@results_table)
-       @table.setPreferredScrollableViewportSize(Dimension.new(1000, 250))
+       @table.setPreferredScrollableViewportSize(Dimension.new(500, 250))
        @table.setFillsViewportHeight(true)
        @table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION)
       
       # Scroll pane
       @js1 = JScrollPane.new(@table)
+      jp2.add(@js1)
       
-      
-      # Buttons
-      chooseDir = JButton.new("choose")
-      searchButton = JButton.new("search")
-      
-      
-      # Add stuff to panels
-     # jp1.add(@tf1)
-      #jp2.add(@tf2)
-      jp3.add(@js1)
-     # jp4.add(l1)
-      #jp5.add(l2)
-      
-      # Add listener actions to button
-      #
-      # Choose Directory Button
-      chooseDir.addActionListener do |e|
-        dir = JFileChooser.new
-        dir.setCurrentDirectory(java.io.File.new(Dir.pwd))
-        dir.setFileSelectionMode(JFileChooser::DIRECTORIES_ONLY)
-        ret = dir.showDialog @panel, "Choose Directory"
-        if  ret  == JFileChooser::APPROVE_OPTION
-            destDir = dir.getCurrentDirectory()
-            searchDir = destDir.to_s
-            @tf1.text = searchDir.length > 0 ? searchDir : ''
-        end 
-      end 
-      
-      # Search button
-      searchButton.addActionListener do |e|
-         if @tf1.text.length <= 0
-           error("Please enter a directory")
-         elsif @tf2.text.length <= 0
-           error("Please enter something to search for")
-         else
-            search
-         end
-      end 
-      
-      
-      
+          
       #
       # GROUP LAYOUT OPTIONS
       #
@@ -202,50 +209,25 @@ class ScaPanel < JPanel
       layout.setAutoCreateContainerGaps true     
      
       sh1 = layout.createSequentialGroup
-      sh2 = layout.createSequentialGroup
-      sh3 = layout.createSequentialGroup
       sv1 = layout.createSequentialGroup
-      sv2 = layout.createSequentialGroup
+      sv2 = layout.createSequentialGroup 
       sv3 = layout.createSequentialGroup
-      p1 = layout.createParallelGroup
-      p2 = layout.createParallelGroup
-      p3 = layout.createParallelGroup
+      p1 = layout.createParallelGroup 
       
-      layout.setHorizontalGroup sh3
-      layout.setVerticalGroup sv3
+      layout.setHorizontalGroup sh1
+      layout.setVerticalGroup sv3 
       
       
       # Horizontal
-      #p1.addComponent(jp4)
       p1.addComponent(jp1)
-      p1.addComponent(jp5)
-      #@p1.addComponent(jp2)
-      p1.addComponent(jp3)
-      
+      p1.addComponent(jp2)
       sh1.addGroup(p1)
-      p2.addComponent(chooseDir)
-      p2.addComponent(searchButton)
-      sh2.addGroup(p2)
-      sh3.addGroup(sh1)
-      sh3.addGroup(sh2)
       
       # Vertical
-     # sv1.addComponent(jp4)
       sv1.addComponent(jp1)
-      sv1.addComponent(jp5)
-      #sv1.addComponent(jp2)      
- 
-      sv1.addComponent(jp3)
-      sv2.addComponent(chooseDir)
-      sv2.addComponent(searchButton)
-      p3.addGroup(sv1)
-      p3.addGroup(sv2)
-      sv3.addGroup(p3)
-      
-       layout.linkSize SwingConstants::HORIZONTAL, 
-          chooseDir, searchButton
-
-     
+      sv1.addComponent(jp2)
+      sv3.addGroup(sv1)
+       
   end
   
   def error(text)
@@ -298,15 +280,18 @@ class WxfScaPanel < JTabbedPane
    
    def init
     cp = ScaPanel.new
-    self.add("Regexp Search", cp)
+    jpanel = JPanel.new()
+    jpanel.setLayout(BorderLayout.new())
+    jpanel.add(cp, BorderLayout::LINE_START)
+    jpanel2 = JPanel.new
+    jpanel2.setLayout(BorderLayout.new())
+    jpanel2.add(jpanel, BorderLayout::PAGE_START)
+    self.add("String Search", jpanel2)
    end
 end
 
-=begin
+
 #Test code within here
-
-
-=end
 
 class TestFrame < JFrame
   
@@ -317,14 +302,11 @@ class TestFrame < JFrame
   
   def init
      
-    
     smp = WxfScaPanel.new
     
     self.add(smp)
-    
-    self.setJMenuBar menuBar
-    self.setPreferredSize Dimension.new(1300, 900)
     self.pack
+    self.setSize(Dimension.new(1300,900))
     
     self.setDefaultCloseOperation JFrame::EXIT_ON_CLOSE
     self.setLocationRelativeTo nil
@@ -336,4 +318,7 @@ end
 
 end
 
-WxfGui::TestFrame.new
+
+
+#WxfGui::TestFrame.new
+
