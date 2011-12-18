e3dde9ee797490c5004c2e3ca77fa15020aa71e4
Ken Johnson <cktricky@gmail.com>
2011-12-17 21:56:57 -0500

    new table added to track progress visually.

--- jlib/wXfgui/analysis.rb
+++ jlib/wXfgui/analysis.rb
@@ -1,3 +1,4 @@
+require 'wXfgui/analysis/tables'
 require 'wXfgui/analysis/dt_modules'
 require 'wXfgui/analysis/trees'
 require 'wXfgui/analysis/panels'
---
+++ jlib/wXfgui/analysis/tables/dt_analysis_table.rb
@@ -0,0 +1,80 @@
+require 'java'
+
+import javax.swing.table.DefaultTableModel
+
+require 'wXfgui/database'
+
+module WxfGui
+
+  class DtAnalysisTable <  DefaultTableModel
+
+     def initialize
+        super()
+        init
+     end
+     
+     def init
+         self.add_column("id")
+         self.add_column("package")
+         self.add_column("required modules")
+         self.add_column("optional modules")
+         self.add_column("completed")
+         self.add_column("attack probability")
+     end 
+     
+     def isCellEditable(row, col)
+        return false
+     end
+     
+     def delete_all_rows
+        i = 0
+        while i < self.getRowCount()
+           self.removeRow(i)
+           i+1
+        end
+     end
+     
+     def remove_results
+     end
+     
+     def insert_results(*params)
+       #Make sure we make an information window if empty
+       if params.kind_of?(Array) and params.length > 0
+          params.each do |rows|
+            rows.each do |row|
+              file         = row[0]
+              line_number  = row[1]
+              string_match = row[2]
+              extension    = row[3]
+              db_add_string_results(file, line_number, string_match, extension)
+            end 
+          end
+       else
+         #Show an error
+       end
+       update_results_fields
+     end
+     
+      def update_results_fields
+        delete_all_rows
+        table_data = retrieve_string_results
+        #Lets get the results
+        if table_data.kind_of?(Array) and table_data.length > 0
+          table_data.each do |row|
+            self.add_row(row.to_java)
+          end 
+        end   
+      end
+      
+      def restore
+        delete_all_rows
+        update_results_fields
+      end
+     
+     def restore_results
+     end
+     
+  end 
+  
+  
+end
