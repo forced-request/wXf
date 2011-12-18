e32e6cfcccd1c03e305835248d7853768626a535
Ken Johnson <cktricky@gmail.com>
2011-12-18 18:00:10 -0500

    tabbed pane and panels added

--- jlib/wXfgui/analysis/panels.rb
+++ jlib/wXfgui/analysis/panels.rb
@@ -1,3 +1,5 @@
 require 'wXfgui/analysis/panels/analysis_panel'
 require 'wXfgui/analysis/panels/dt_analysis_panel'
+require 'wXfgui/analysis/panels/details_panel'
+require 'wXfgui/analysis/panels/info_panel'
 
--- jlib/wXfgui/analysis/tabs/dt_tabs.rb
+++ jlib/wXfgui/analysis/tabs/dt_tabs.rb
@@ -7,6 +7,15 @@ module WxfGui
   class DtTabs < JTabbedPane
     
     def initialize 
+      super(JTabbedPane::TOP, JTabbedPane::SCROLL_TAB_LAYOUT)
+      add_tabs 
+    end
+    
+    def add_tabs
+      @details  =   DetailsPanel.new()
+      @info  = InfoPanel.new()
+      add("Module Details", @details)
+      add("Information", @info)
     end
     
   end
