b7b15c094068ea4f7319ef00546b7da94476ff46
Ken Johnson <cktricky@gmail.com>
2011-12-18 19:08:50 -0500

    Essentially, need a next and analyze button. This is to help manage the flow of the application.

---
+++ jlib/wXfgui/analysis/panels/dt_table_panel.rb
@@ -0,0 +1,78 @@
+require 'java'
+
+import javax.swing.JPanel
+import javax.swing.JTable
+import java.awt.Color
+
+module WxfGui
+  
+  class DtTablePanel < JPanel
+    
+    def initialize
+      super()
+      init
+    end
+    
+  def init
+      
+      #
+      # GROUP LAYOUT OPTIONS
+      #
+      
+      layout = GroupLayout.new self
+      # Add Group Layout to the frame
+      self.setLayout layout
+      # Create sensible gaps in components (like buttons)
+      layout.setAutoCreateGaps true
+      layout.setAutoCreateContainerGaps true
+    
+      sh1 = layout.createSequentialGroup
+      sv1 = layout.createSequentialGroup
+      sv2 = layout.createSequentialGroup
+      sv3 = layout.createSequentialGroup
+      p1  = layout.createParallelGroup
+      p2 = layout.createParallelGroup
+      
+      layout.setHorizontalGroup sh1
+      layout.setVerticalGroup sv1
+    
+      
+      @dat = DtAnalysisTable.new
+      @dat_jtable = JTable.new(@dat)
+      @dat_jtable.show_vertical_lines = true
+      @dat_jtable.show_grid = true
+      @dat_jtable.grid_color = Color.lightGray
+     
+      
+      @js1 = JScrollPane.new(@dat_jtable)
+      
+      @next_button  = JButton.new("next")
+      @analyze_button  = JButton.new("analyze")
+      
+      @next_button.add_action_listener do |e|
+        
+      end
+      
+      @analyze_button.add_action_listener do |e|
+      
+      end
+      
+      p1.addComponent(@next_button)
+      p1.addComponent(@analyze_button)
+      sh1.addComponent(@js1)
+      sh1.addGroup(p1)
+      
+      sv2.addComponent(@js1)
+      sv3.addComponent(@next_button)
+      sv3.addComponent(@analyze_button)
+      p2.addGroup(sv2)
+      p2.addGroup(sv3)
+      sv1.addGroup(p2)
+      
+      layout.linkSize SwingConstants::HORIZONTAL, 
+          @next_button, @analyze_button
+      
+  end
+    
+  end
+end
